e1887a11e99e4b75d78fe97917c09e7dc8cad820
hdhimal2020 <73497433+hdhimal2020@users.noreply.github.com>
2021-03-28 01:00:42 -0400

    Update visualizationPart3.py
    Hari Added code for line chart from line 55-63

--- Plots/visualizationPart3.py
+++ Plots/visualizationPart3.py
@@ -52,6 +52,15 @@ data_stackbarchart = [trace1_stackbarchart, trace2_stackbarchart, trace3_stackba
 line_df = df4
 line_df['date'] = pd.to_datetime(line_df['date'])
 data_linechart = [go.Scatter(x=df4['date'], y=df4['actual_max_temp'], mode='lines', name='Max Temperature')]
+html.H3('Line chart', style={'color': '#df1e56'}),
+html.Div('This line chart represent the Maximum Temperature of each day of the month in the given period.'),
+dcc.Graph(id='graph4',
+              figure={
+                  'data': data_linechart,
+                  'layout': go.Layout(title='Maximum Temperature of each day of the month From 2014-07-01 to 2015-6-15',
+                                      xaxis={'title': 'Month'}, yaxis={'title': 'Max Temperature'})
+              }
+              ),
 
 # 4 Multi Line Chart - Kush Bhuva
 
